fa5a98861aca36df5aa73f8a373cd8f8af561485
ronaldreagan <gamitinmoangemail@gmail.com>
2024-08-06 11:05:09 +0800

    update

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/send', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'gamitinmoangemail@gmail.com',
+      pass: 'Mamamodaw2'
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'gamitinmoangemail@gmail.com',
+    subject: `Contact form submission from ${name}`,
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send('Error sending email');
+    }
+    res.status(200).send('Email sent successfully');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
